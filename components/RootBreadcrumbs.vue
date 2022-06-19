136fd499e90ee4f73b07abaf12d22e0c00f437cc
Dan Trickey <git@trickey.io>
2022-06-19 10:45:00 +0100

    Remove rootbreadcrubs

--- components/RootBreadcrumbs.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section>
-    <b-breadcrumb :align="this.align">
-      <b-breadcrumb-item tag="router-link" to="/" active
-        >Inventory</b-breadcrumb-item
-      >
-    </b-breadcrumb>
-  </section>
-</template>
--- pages/nodes/index.vue
+++ pages/nodes/index.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="section">
     <h1 class="title is-1">Assets</h1>
-    <root-breadcrumbs />
     <node-table parent="root" />
   </section>
 </template>
