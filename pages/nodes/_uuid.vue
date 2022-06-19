42f0f6705793952fdf50b30294bd8ccb69f11de4
Dan Trickey <git@trickey.io>
2022-06-19 12:05:05 +0100

    Add node icons in Asset page titles

--- pages/nodes/_uuid.vue
+++ pages/nodes/_uuid.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="section">
     <h1 class="title is-1">
-      {{ node.display_name }}
+      <node-icon :node="node" />&nbsp;{{ node.display_name }}
     </h1>
     <node-breadcrumbs :node="node" />
     <node-info :node="node" />
@@ -10,9 +10,7 @@
 </template>
 
 <script>
-import NodeBreadcrumbs from '~/components/NodeBreadcrumbs.vue'
 export default {
-  components: { NodeBreadcrumbs },
   async asyncData({ $axios, params }) {
     const node = await $axios.$get('nodes/' + params.uuid)
     return { node }
