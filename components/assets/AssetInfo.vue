35d7ca9b280fbf1c03837e93521ea4cd6738cbc2
Dan Trickey <git@trickey.io>
2022-06-25 12:30:37 +0100

    Render state of asset nicely in AssetInfo

--- components/assets/AssetInfo.vue
+++ components/assets/AssetInfo.vue
@@ -10,7 +10,7 @@
       </div>
       <div class="column">
         <h5 class="title is-5">State</h5>
-        <p>{{ asset.state }}</p>
+        <p><asset-state-string :state="asset.state" /></p>
         <h5 class="title is-5">Last Updated</h5>
         <p>{{ asset.updated_at }}</p>
       </div>
